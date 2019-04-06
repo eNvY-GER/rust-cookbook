cf5a62400ed422135580dbcb7bf989e12e3f42ab
eNvY-GER <Leif_G@web.de>
2019-04-06 23:34:27 +0200

    first snippet for hello_world added, added example form of user_input but without comparison for now

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,10 @@
+//use statement is like an import statement in java
+use chrono::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    //let is the way variables are defined in Rust
+    let now: DateTime<Local> = Local::now();
+    //there are different formatting strings for variables which do not implement the display trait ({} does not work)
+    //:#? is the pretty print formatter while :? is the debug formatter
+    println!("Hello world from Rust! It is {:#?}", now.weekday());
 }
--- user_input/src/main.rs
+++ user_input/src/main.rs
@@ -1,3 +1,15 @@
+use chrono::prelude::*;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Which day do we have today?");
+    let mut day = String::new();
+    io::stdin()
+        .read_line(&mut day)
+        .expect("Failed to read froms standard in");
+    println!(
+        "You have said: {}The correct answer is {:#?}",
+        day,
+        Local::now().weekday()
+    );
 }
